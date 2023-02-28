938bd8e79214e3c31df8e8048fe3bd8860487afd
ElMourabit-Mohammed <elmourabitmohammed00@gmail.com>
2023-02-28 20:30:47 +0100

    JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 
 function App() {
+  let name="El mourabit Mohammed";
+  let num1=12;
+  let num2=8;
 return (
 <div className="App">
   <h1>Hello World!</h1>
+
+  <label htmlFor="name">Hello {name}</label>
+  <input type="text" name="name" id="" />
+  
+  <p>{num1}+{num2}={num1+num2}</p>
 </div>
 );
 }
